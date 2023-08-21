3a197bb03065c7235187bf84757dfe3c29fac48b
jdutton8 <jennydutton7@gmail.com>
2023-08-21 16:47:44 -0500

    added imports to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 import { useState } from 'react'
 import './App.css'
+import AboutMe from './pages/AboutMe'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+import Resume from './pages/Resume'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [displayState, setDisplayState] = useState('')
+
+  const DisplaySwitch = (state) => {
+    switch (state) {
+      case '':
+        return <AboutMe setDisplayState = { setDisplayState } />
+      case 'projects':
+        return <Projects />
+      case 'contact':
+        return <Contact />
+      case 'resume':
+        return <Resume />
+      default:
+        return <AboutMe />
+    }
+  }
 
   return (
     <>
-      
+       <Navbar setDisplayState = { setDisplayState } />
+      {DisplaySwitch(displayState)}
+      <Footer setDisplayState = { setDisplayState } />
     </>
   )
 }

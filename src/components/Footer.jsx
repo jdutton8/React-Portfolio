95196a50a4e588e0378f5d5f29a9a275262c5c19
jdutton8 <jennydutton7@gmail.com>
2023-08-21 21:37:42 -0500

    added links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="footer">
+    <div id="footer-content">
+      <a href="https://github.com/jdutton8">
+        <img className="logo" src="/github-logo.png" />
+      </a>
+      <a href="https://www.linkedin.com/in/jenny-dutton-963924282/">
+        <img className="logo" src="/linkedIn-logo.png" />
+      </a>
+    </div>
+  </footer>
   )
 }
 

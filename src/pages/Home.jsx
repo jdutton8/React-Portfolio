ab7dd18e033dc6f621801ec6bff7991120f75974
jdutton8 <jennydutton7@gmail.com>
2023-08-21 19:59:52 -0500

    added about me info

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="about-me">
+    <img className="aboutme-photo" src="/jenny.jpg" alt="A photo of the developer"/>
+    <span className="about-text">
+      <p className="about-header">About Me</p>
+      Hello! My name is Jenny. I'm a Full-Stack Developer,
+      busy mom of seven, and Harry Potter enthusiast! Being a mom is my biggest joy and an incredible adventure. 
+      Alongside my role as a mom, I have a passion for coding. I find immense satisfaction in bringing ideas to life through programming 
+      and creating innovative solutions. Coding allows me to exercise my creativity and problem-solving skills, and it's a constant source of excitement 
+      and learning for me. Whether it's building websites, developing apps, or exploring new technologies, I immerse myself in the world of coding with 
+      enthusiasm. I believe that technology has the power to shape the future, and I'm thrilled to be a part of it. 
+      Join me on this journey as I navigate the joys of motherhood and dive into the captivating world of coding!
+    </span>
+  </div>
   )
 }
 
